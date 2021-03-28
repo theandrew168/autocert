a6cc9b9c3bc0ad97e97a1464085724abf59aca49
theandrew168 <steveday168@gmail.com>
2021-03-28 12:47:35 -0500

    Bump version to 0.0.6 and adjust classifiers

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md') as f:
 
 setup(
     name='autocert',
-    version='0.0.5',
+    version='0.0.6',
     author='Andrew Dailey',
     description='Automatic TLS cert issuance and renewal for Python web apps',
     long_description=readme,
@@ -18,9 +18,15 @@ setup(
         'requests',
     ],
     classifiers=[
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-        "Programming Language :: Python :: 3",
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Topic :: Internet :: WWW/HTTP :: HTTP Servers',
+        'Topic :: System :: Networking',
     ],
     python_requires='>=3.0',
 )
